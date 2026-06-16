90ff006bc44db937c363da5d2ec68cc0bedbda74
will-last <2584329483@qq.com>
2026-06-16 20:31:49 +0800

    update

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,35 +1,38 @@
 <template>
-    <div class="container">
-        <div class="title">
-            <div class="back-home">
-                <el-icon><Back /></el-icon>
-                <span>返回首页</span>
-            </div>
-            <div class="title-text">
-                <h2>登录您的账户</h2>
-                <p>请输入您的登录信息</p>
-            </div>
-        </div>
-        <div class="form-container">
-            <el-form
-                ref="reluFormRef"
-                :model="formData"
-                :rules="rules"
-                label-position="top"
-            >
-                <el-form-item label="用户名或邮箱" prop="username">
-                    <el-input v-model="formData.username" size="large" placeholder="请输入用户名" />
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input v-model="formData.password" size="large" placeholder="请输入密码" type="password" show-password />
-                </el-form-item>
-                <el-button class="btn" type="primary" size="large" @click="submitForm(reluFormRef)">登录</el-button>
-            </el-form>
-        </div>
-        <div class="footer">
-            <p>还没有账户？<router-link to="/auth/register">去注册</router-link></p>
-        </div>
+  <div class="container">
+    <div class="title">
+      <div class="back-home">
+        <el-icon><Back /></el-icon>
+        <span style="cursor: pointer;" @click="() => {router.push('/')}">返回首页</span>
+      </div>
+      <div class="title-text">
+        <h2>登录您的账户</h2>
+        <p>请输入您的登录信息</p>
+      </div>
     </div>
+    <div class="form-container">
+      <el-form ref="reluFormRef" :model="formData" :rules="rules" label-position="top">
+        <el-form-item label="用户名或邮箱" prop="username">
+          <el-input v-model="formData.username" size="large" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="formData.password"
+            size="large"
+            placeholder="请输入密码"
+            type="password"
+            show-password
+          />
+        </el-form-item>
+        <el-button class="btn" type="primary" size="large" @click="submitForm(reluFormRef)"
+          >登录</el-button
+        >
+      </el-form>
+    </div>
+    <div class="footer">
+      <p>还没有账户？<router-link to="/auth/register">去注册</router-link></p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -40,69 +43,65 @@ import { useRouter } from 'vue-router'
 const reluFormRef = ref()
 
 const formData = reactive({
-    username: '',
-    password: ''
+  username: '',
+  password: '',
 })
 const rules = reactive({
-    username: [
-        { required: true, message: '请输入用户名或邮箱', trigger: 'blur' }
-    ],
-    password: [
-        { required: true, message: '请输入密码', trigger: 'blur' }
-    ]
+  username: [{ required: true, message: '请输入用户名或邮箱', trigger: 'blur' }],
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
 })
 
 const router = useRouter()
 const submitForm = async (formEl) => {
-    if(!formEl) return
-    await formEl.validate((valid, fields) => {
-        if(valid) {
-            login(formData).then(data => {
-                if(!data.token){
-                    return console.error('登录失败')
-                }
-                localStorage.setItem('token', data.token)
-                localStorage.setItem('userInfo', JSON.stringify(data.userInfo))
-                if(data.userInfo.userType === 2) {
-                    router.push('/back/dashboard')
-                }else{
-                    router.push('/')
-                }
-            })
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      login(formData).then((data) => {
+        if (!data.token) {
+          return console.error('登录失败')
         }
-    })
+        localStorage.setItem('token', data.token)
+        localStorage.setItem('userInfo', JSON.stringify(data.userInfo))
+        if (data.userInfo.userType === 2) {
+          router.push('/back/dashboard')
+        } else {
+          router.push('/')
+        }
+      })
+    }
+  })
 }
 </script>
 
 <style lang="scss" scoped>
-    .container {
-        width: 384px;
-        .title {
-            .back-home {
-                margin-bottom: 60px;
-            }
-            .title-text {
-                text-align: center;
-                h2 {
-                    font-size: 36px;
-                    margin-bottom: 10px;
-                }
-                p {
-                    font-size: 18px;
-                    color: #6b7280;
-                }
-            }
-        }
-        .form-container {
-            margin-top: 30px;
-            .btn {
-                width: 100%;
-                margin-top: 40px;
-            }
-        }
-        .footer {
-            padding: 30px;
-            text-align: center;
-        }
+.container {
+  width: 384px;
+  .title {
+    .back-home {
+      margin-bottom: 60px;
     }
+    .title-text {
+      text-align: center;
+      h2 {
+        font-size: 36px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 18px;
+        color: #6b7280;
+      }
+    }
+  }
+  .form-container {
+    margin-top: 30px;
+    .btn {
+      width: 100%;
+      margin-top: 40px;
+    }
+  }
+  .footer {
+    padding: 30px;
+    text-align: center;
+  }
+}
 </style>
