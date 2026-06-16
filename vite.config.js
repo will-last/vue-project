d7bf1adfdaac1b2b4bc01437b01456fa4b59c87d
will-last <2584329483@qq.com>
2026-06-16 18:38:07 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-project/',
+  base: '/vue-project/auth/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
